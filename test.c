1ecb186d722550beb2302aebf8d35436a3b5fcb3
Maksym Zaitsev <cryptolok@protonmail.com>
2017-12-15 19:58:46 +0100

    x32 DEP/NX bypass

--- test.c
+++ test.c
@@ -4,22 +4,21 @@
 
 //$ sudo gcc -z execstack test.c -o test
 //$ sudo gcc -m32 -z execstack test.c -o test32
-//$ sudo chmod +s test test32
+//$ sudo gcc -m32 test.c -o test32x
+//$ sudo chmod +s test test32 test32x
 
 void showInput(char *arg)
 {
     char buffer[1024];
     strcpy(buffer,arg);
-    puts("IN ASLR WE TRUST!");
+    puts("IN DEP/ASLR WE TRUST!");
 }
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
-		printf("PUT SHELLCODE INTO ENVIRONMENT!\n");
+		printf("EXPLOIT THE ARGUMENT!\n");
 	else
 		showInput(argv[1]);
 	return 1;
 }
-
-
